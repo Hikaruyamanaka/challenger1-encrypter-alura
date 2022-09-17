bad091d0236ea7b8a79eb6699880b6fda2385781
Hikaru <hikaru.yamanaka@outlook.com>
2022-09-17 14:40:11 -0300

    Challenger1 Encrypter

---
+++ js/index.js
@@ -0,0 +1,65 @@
+var inputText = document.querySelector(".decoded-text");
+var paragraph = document.querySelector(".text-result-paragraph");
+var outputText = document.querySelector(".textarea-result-paragraph");
+var encryptButton = document.querySelector(".encrypt-button");
+var decryptButton = document.querySelector(".decrypt-button");
+var copyButton = document.querySelector(".copy-button");
+
+function encrypt() {
+    var encryptedText = inputText.value.toLowerCase();
+
+    var result = encryptedText
+
+        .replaceAll("e", "enter")
+        .replaceAll("i", "imes")
+        .replaceAll("a", "ai")
+        .replaceAll("o", "ober")
+        .replaceAll("u", "ufat");
+
+    outputText.value = result;
+
+    document.querySelector(".img-text-result").style.display = "none";
+    document.querySelector(".text-result-title").style.display = "none";
+    document.querySelector(".text-result-paragraph").style.display = "none";
+    document.querySelector(".textarea-result-paragraph").innerHTML = result;
+    document.querySelector(".textarea-result-paragraph").style.display = "block";
+    document.querySelector(".copy-button").style.display = "block";
+}
+
+function decrypt() {
+    var decryptedText = inputText.value.toLowerCase();
+
+    var result = decryptedText
+
+        .replaceAll("enter", "e")
+        .replaceAll("imes", "i")
+        .replaceAll("ai", "a")
+        .replaceAll("ober", "o")
+        .replaceAll("ufat", "u");
+
+    outputText.value = result;
+
+    document.querySelector(".img-text-result").style.display = "none";
+    document.querySelector(".text-result-title").style.display = "none";
+    document.querySelector(".text-result-paragraph").style.display = "none";
+    document.querySelector(".textarea-result-paragraph").innerHTML = result;
+    document.querySelector(".textarea-result-paragraph").style.display = "block";
+    document.querySelector(".copy-button").style.display = "block";
+}
+
+function copyToClipboard() {
+    var text = document.querySelector(".decoded-text");
+    var textContent = document.querySelector(".textarea-result-paragraph").innerHTML;
+
+    navigator.clipboard.writeText(textContent);
+
+    text.value = "";
+
+    setTimeout(function () {
+        location.reload();
+    }, 800);
+}
+
+encryptButton.addEventListener("click", encrypt);
+decryptButton.addEventListener("click", decrypt);
+copyButton.addEventListener("click", copyToClipboard);
