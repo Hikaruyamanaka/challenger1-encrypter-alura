17b82edadd4a3af2697091cc04f3529e6226ab8f
Hikaru <hikaru.yamanaka@outlook.com>
2022-09-17 20:10:08 -0300

    Finalizado

--- js/script.js
+++ js/script.js
@@ -5,59 +5,78 @@ var encryptButton = document.querySelector(".encrypt-button");
 var decryptButton = document.querySelector(".decrypt-button");
 var copyButton = document.querySelector(".copy-button");
 
+
+var input = document.querySelector(".decoded-text");
+input.addEventListener("keypress", function(e) {
+    if(!checkChar(e)) {
+      e.preventDefault();
+  }
+});
+
+function checkChar(e) {
+    var char = String.fromCharCode(e.keyCode);
+
+    var pattern = '[a-z]';
+    if (char.match(pattern)) {
+      return true;
+  }
+}
+
 function encrypt() {
-    var encryptedText = inputText.value.toLowerCase();
+  var encryptedText = inputText.value.toLowerCase();
 
-    var result = encryptedText
+  var result = encryptedText
 
-        .replaceAll("e", "enter")
-        .replaceAll("i", "imes")
-        .replaceAll("a", "ai")
-        .replaceAll("o", "ober")
-        .replaceAll("u", "ufat");
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
 
-    outputText.value = result;
+  outputText.value = result;
 
-    document.querySelector(".img-text-result").style.display = "none";
-    document.querySelector(".text-result-title").style.display = "none";
-    document.querySelector(".text-result-paragraph").style.display = "none";
-    document.querySelector(".textarea-result-paragraph").innerHTML = result;
-    document.querySelector(".textarea-result-paragraph").style.display = "block";
-    document.querySelector(".copy-button").style.display = "block";
+  document.querySelector(".img-text-result").style.display = "none";
+  document.querySelector(".text-result-title").style.display = "none";
+  document.querySelector(".text-result-paragraph").style.display = "none";
+  document.querySelector(".textarea-result-paragraph").innerHTML = result;
+  document.querySelector(".textarea-result-paragraph").style.display = "block";
+  document.querySelector(".copy-button").style.display = "block";
 }
 
 function decrypt() {
-    var decryptedText = inputText.value.toLowerCase();
+  var decryptedText = inputText.value.toLowerCase();
 
-    var result = decryptedText
+  var result = decryptedText
 
-        .replaceAll("enter", "e")
-        .replaceAll("imes", "i")
-        .replaceAll("ai", "a")
-        .replaceAll("ober", "o")
-        .replaceAll("ufat", "u");
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
 
-    outputText.value = result;
+  outputText.value = result;
 
-    document.querySelector(".img-text-result").style.display = "none";
-    document.querySelector(".text-result-title").style.display = "none";
-    document.querySelector(".text-result-paragraph").style.display = "none";
-    document.querySelector(".textarea-result-paragraph").innerHTML = result;
-    document.querySelector(".textarea-result-paragraph").style.display = "block";
-    document.querySelector(".copy-button").style.display = "block";
+  document.querySelector(".img-text-result").style.display = "none";
+  document.querySelector(".text-result-title").style.display = "none";
+  document.querySelector(".text-result-paragraph").style.display = "none";
+  document.querySelector(".textarea-result-paragraph").innerHTML = result;
+  document.querySelector(".textarea-result-paragraph").style.display = "block";
+  document.querySelector(".copy-button").style.display = "block";
 }
 
 function copyToClipboard() {
-    var text = document.querySelector(".decoded-text");
-    var textContent = document.querySelector(".textarea-result-paragraph").innerHTML;
+  var text = document.querySelector(".decoded-text");
+  var textContent = document.querySelector(
+    ".textarea-result-paragraph"
+  ).innerHTML;
 
-    navigator.clipboard.writeText(textContent);
+  navigator.clipboard.writeText(textContent);
 
-    text.value = "";
+  text.value = "";
 
-    setTimeout(function () {
-        location.reload();
-    }, 800);
+  setTimeout(function () {
+    location.reload();
+  }, 800);
 }
 
 encryptButton.addEventListener("click", encrypt);
